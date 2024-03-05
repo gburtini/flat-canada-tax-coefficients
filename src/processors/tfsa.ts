66419111d6ad0bc0ae8f5b2b4ef139506bed9f95
Giuseppe Burtini <giuseppe@burtini.ca>
2024-03-05 09:57:37 -0800

    Update tfsa.ts - revised column headers.

--- src/processors/tfsa.ts
+++ src/processors/tfsa.ts
@@ -1,7 +1,7 @@
 import { runner, defaultProcessor, defaultCleaner } from "../lib/runner.ts";
 
-const TABLE_HEADER = "TFSA dollar";
+const TABLE_HEADER = "TFSA and ALDA dollar limits";
 const selector = `table:has(caption>strong:contains('${TABLE_HEADER}'))`;
-const keys: string[] = ["year", "limit"];
+const keys: string[] = ["year", "limit", "alda"];
 
 await runner(Deno.args[0], defaultProcessor(selector, keys), defaultCleaner());
