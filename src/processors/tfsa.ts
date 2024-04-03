6efc7cda1d6cdca8eb81b943fdedd550a8baec16
Giuseppe Burtini <giuseppe@burtini.ca>
2024-04-03 16:43:04 -0700

    Remove silly comments.

--- src/processors/tfsa.ts
+++ src/processors/tfsa.ts
@@ -1,7 +1,7 @@
 import { runner, defaultProcessor, defaultCleaner } from "../lib/runner.ts";
 
-const TABLE_HEADER = "TFSA and ALDA dollar limits"; // Updated table header to match the webpage
+const TABLE_HEADER = "TFSA and ALDA dollar limits"; 
 const selector = `caption:contains('${TABLE_HEADER}')`;
-const keys: string[] = ["year", "TFSA dollar limit"]; // Updated to match the column names in the table
+const keys: string[] = ["year", "TFSA dollar limit"];
 
 await runner(Deno.args[0], defaultProcessor(selector, keys), defaultCleaner());
